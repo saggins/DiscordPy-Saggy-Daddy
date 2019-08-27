15edbdb816e92fec282d05ed4a8bca39f03c5a1f
win32prog <win32prog@gmail.com>
2019-08-26 19:00:20 -0700

    logging in works

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # DiscordPy-Saggy-Daddy
 A discord bot that I have.
+
+Put token at top line in /saggy-daddy/secretstuff.txt
--- saggy-daddy/core.py
+++ saggy-daddy/core.py
@@ -1,11 +1,14 @@
 import discord 
+import asyncio
+import os
 
-secretfile = open("secretstuff.txt", "r")
-token = secretfile.read(0)
+secretfile = open(os.getcwd()+"/saggy-daddy/secretstuff.txt", "r")
+token = secretfile.read()
 secretfile.close()
 
 class SaggyDaddy(discord.Client):
-    async def on_ready(self):
+    @asyncio.coroutine
+    def on_ready(self):
         print('Logged on as {0}!'.format(self.user))
 
 client = SaggyDaddy()
