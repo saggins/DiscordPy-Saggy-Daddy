2edde55754082f921142f05e7dad51acee1e92e7
win32prog <win32prog@gmail.com>
2019-09-04 20:20:35 -0700

    things i did, i think

--- saggy-daddy/core.py
+++ saggy-daddy/core.py
@@ -2,6 +2,7 @@ import discord
 import asyncio
 import os
 import Dmcping
+import sched, time
 
 secretfile = open(os.getcwd()+"/saggy-daddy/secretstuff.txt", "r")
 token = secretfile.read()
@@ -9,9 +10,19 @@ secretfile.close()
 
 class SaggyDaddy(discord.Client):
     @asyncio.coroutine
+    #wipe server status channel
+    # send the is it  up message
+    # perodxly wipe and write new message if things have changed
     def on_ready(self):
+        
+        channel = discord.utils.get(client.get_all_channels(), guild__name='Team Speak', name='server-status')
+        async def pruge(channel):
+            
+        pruge(channel)
+        
         print('Logged on as {0}!'.format(self.user))
     
+    s = sched.scheduler(time.time, time.sleep)
 
 client = SaggyDaddy()
 client.run(token)
