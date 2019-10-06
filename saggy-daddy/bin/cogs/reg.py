183dc28d682989f6da5102062054752e4d9a082d
win32prog <win32prog@gmail.com>
2019-10-05 20:36:56 -0700

    Finished step1 logic

--- saggy-daddy/bin/cogs/reg.py
+++ saggy-daddy/bin/cogs/reg.py
@@ -15,17 +15,42 @@ class reg(commands.Cog):
                 'userid':ctx.author.id,
             }
         ) 
+
         try:
             step1=step['Item']
         except:
             await ctx.send(ctx.author.mention + " You aren't in database?. **Pls Tell server Mod!**")
 
-        if(step1['step']==0):
+        #Begining Logic
+        if(step1['reg'] == 0): #Is it step 0?
             if(len(args)!=1):
                 await ctx.send('Please do $reg **Your name**')
             else:
-                pass
+                await self.updatestuff(ctx.author.id, 1, 'name', args[0] )
                 #TODO: Finish the logic system for regestration
-            
+    
+    
+    async def updatestuff(self, id, newstep, key, value):
+        table.update_item( #update reg
+            Key={
+                'userid': id,
+            },
+            UpdateExpression='SET reg = :vali1',
+            ExpressionAttributeValues={
+                ':vali1': newstep
+            }
+        )     
+        table.update_item(
+            Key={
+                'userid': id,
+            },
+            UpdateExpression='SET #key = :vali1',
+            ExpressionAttributeNames={
+                '#key' : key
+            },
+            ExpressionAttributeValues={
+                ':vali1': value,
+            }
+        )     
 def setup(bot):
     bot.add_cog(reg(bot))
